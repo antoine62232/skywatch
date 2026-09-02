87597d35f8825ee71e4023844080d2ede6489623
antoine62232 <antoinebayart@laposte.net>
2026-09-02 14:24:49 +0200

    init : squelette Streamlit qui démarre

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Supervision trafic aérien", layout="wide")
+
+st.title("Console de supervision du trafic aérien en temps réel")
+st.write("L'application tourne.")
